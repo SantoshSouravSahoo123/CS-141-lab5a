91a9aabb0cfb05b986ddc2d078623b4e0d0a758f
SantoshSouravSahoo123 <42238175+SantoshSouravSahoo123@users.noreply.github.com>
2018-09-06 19:52:52 +0530

    Add files via upload

---
+++ lab5_q1.cpp
@@ -0,0 +1,22 @@
+//first include library
+#include<iostream>
+using namespace std;
+int main()
+	{
+	//declaring the variables
+	int  n;
+	cout<<" Enter the number of rows"<<endl;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	// print the stars
+	for( int i=0;i<n;i++)
+	{
+		cout<<"*";
+	}
+	cout<<endl;
+	}
+
+	return 0;
+}
+
